f4724f1c4461d51f0108863ac6c91b8f576a2370
dongkyunkimdev <ddkds66@gmail.com>
2023-01-06 18:16:36 +0900

    Configuration open feign

---
+++ src/main/java/com/example/address/config/FeignConfig.java
@@ -0,0 +1,9 @@
+package com.example.address.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.example")
+public class FeignConfig {
+}
